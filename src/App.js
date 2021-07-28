313ebf7c83a7e89761ac61023af4ea7d8e3438a9
Sean Narron <seannarron9@gmail.com>
2021-07-28 14:19:27 +0900

    Created state instead of static

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Tweet from "./Tweet";
 
 function App() {
+
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+
   return (
     <div className="app">
-      <Tweet name="Sean"/>
-      <Tweet name="Jack"/>
-      <Tweet name="Jane"/>
-      <Tweet name="Ed"/>
+      <Tweet name="Sean" message="This is a random tweet"/>
+      <Tweet name="Jack" message="What's up dudes?"/>
+      <Tweet name="Jane" message="Having a burger here"/>
+      <Tweet name="Ed" message="Cold coffee sucks"/>
+
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
       </div>
   )
 }
