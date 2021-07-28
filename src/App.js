9f53272763142090d21352b5a45c0c3325d46809
Sean Narron <seannarron9@gmail.com>
2021-07-28 09:49:57 +0900

    added a Tweet component (static) with some styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tweet from "./Tweet";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+function App() {
+  return (
+    <div className="app">
+      <Tweet name="Sean"/>
+      <Tweet name="Jack"/>
+      <Tweet name="Jane"/>
+      <Tweet name="Ed"/>
       </div>
-    );
-  }
+  )
 }
 
 export default App;
