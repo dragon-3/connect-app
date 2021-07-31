a16f9dc76b8d34262d67b1476e36470c20b9aa9d
Sean Narron <seannarron9@gmail.com>
2021-07-31 11:54:29 +0900

    Edited for practice

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,9 +4,8 @@ function SearchBar(props) {
     return(
         <div className="search-box">
             <div className="search-bar">
-                <form action="/action_page.php">
-                <label for="fname"></label>
-                    <input type="text" id="fname" name="fname" placeholder="    What's on your mind?"></input>
+                <form action="">
+                    <input type="text" placeholder="    What's on your mind??"></input>
                 </form>
             </div>
         </div>
