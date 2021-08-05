1c5e08279bbced0a49f0392caa4cdff4be336a77
Sean Narron <seannarron9@gmail.com>
2021-08-05 15:27:20 +0900

    dk

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -5,7 +5,7 @@ function SearchBar(props) {
         <div className="search-box">
             <div className="search-bar">
                 <form action="/action_page.php">
-                <label for="fname"></label>
+                    <label for="fname"></label>
                     <input type="text" id="fname" name="fname" placeholder="    What's on your mind?"></input>
                 </form>
             </div>
