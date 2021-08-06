47605e2c14c89e8a75efe9441f52a539e999728e
Sean Narron <seannarron9@gmail.com>
2021-08-06 17:17:30 +0900

    Added some intro text

--- src/NewPost.js
+++ src/NewPost.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const NewPost = (props) => {
     return(
+        
         <div className="post-box">
             <div className="post">
                 <form >
-                    <label for="fname"></label>
+                    <div className="post-intro">
+                        <p>Create a post</p>
+                    </div>
                     <input type="text" id="fname" name="fname" placeholder="    Type here"></input>
                 </form>
             </div>
