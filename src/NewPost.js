f3cf48dc483d80896a87393a00650b8b7c49a786
Sean Narron <seannarron9@gmail.com>
2021-08-09 19:27:59 +0900

    Created an input state function

--- src/NewPost.js
+++ src/NewPost.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import { render, Compnent } from 'react-dom';
 
-const NewPost = (props) => {
-    return(
+class NewPost extends React.Component {
+
+    constructor () {
+        super();
+        this.state = {
+            fullName: null
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
+    handleInputChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    render() {
+        const fullName = this.state
+
+        return(
         
-        <div className="post-box">
-            <div className="post">
-                <form >
-                    <input type="text" id="fname" name="fname" placeholder="    Type here"></input>
-                </form>
+            <div className="post-box">
+                <div className="post">
+                    <form >
+                        <input type="text" id="name" name="fullName" onChange={this.handleInputChange} placeholder="    Type here"></input>
+                    </form>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    
 }
 
 export default NewPost;
